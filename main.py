0679749a0591cdbe024024cb53541361d6786ff7
Zetra-Dev <zetranova009@gmail.com>
2025-03-17 15:33:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 import uvicorn
 
-# Cargar variables de entorno desde el archivo .env
+# Carregar variáveis de ambiente
 load_dotenv()
 
 app = FastAPI()
@@ -19,33 +19,38 @@ class MusicRequest(BaseModel):
     guild_id: str
     query: str
 
+class LeaveRequest(BaseModel):
+    guild_id: str
+
 @app.on_event("startup")
 async def startup_event():
-    # Inicia el bot de Discord
+    # Inicia o bot de Discord
     asyncio.create_task(music_bot.start_bot())
 
 @app.post("/play-music")
 async def play_music(request: MusicRequest):
     try:
-
         m = await music_bot.play_music(request.user_id, request.channel_id, request.guild_id, request.query)
         return JSONResponse({"message": m}, status_code=200)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-
 
 @app.get("/music-queue")
 async def music_queue():
     try:
         queue = await music_bot.show_queue()
         return {"queue": queue}
-    
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+@app.post("/leave-music")
+async def leave_music(request: LeaveRequest):
+    try:
+        m = await music_bot.leave_voice_channel(request.guild_id)
+        return JSONResponse({"message": m}, status_code=200)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 if __name__ == "__main__":
-    # Inicia la aplicación FastAPI
+    # Inicia a aplicação FastAPI
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
